2e67eeab5c2e905311d6a08a4076b238e1ead33c
Alex Andre Romanhuk <aaromanhuk@coamo.com.br>
2025-11-21 14:28:16 -0300

    Montado a tela do pomodoro

--- react-course/src/App.tsx
+++ react-course/src/App.tsx
@@ -1,6 +1,8 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+import { CountDown } from './components/CountDown';
+import { Input } from './components/Input';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 import './styles/global.css';
 import './styles/theme.css'
@@ -12,7 +14,37 @@ export function App() {
                 <Logo/>                
             </Container>
             <Container>
-                <Heading>MENU</Heading>
+                <Menu/>
+             </Container>
+            <Container>
+                <CountDown/>
+             </Container>
+            <Container>
+                <form action="form">
+
+                    <div className='formRow'>
+                        <Input labelText='Task' 
+                            id='menuInput' 
+                            type='text' 
+                            placeholder='Digite algo'
+                            disabled
+                            defaultValue='Valor preenchido'/>
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0 0</p>
+                    </div>
+
+                    <div className='formRow'>
+                        <button type="submit">Enviar</button>
+                    </div>
+
+                </form>
              </Container>
         </>
     );
