392e5d94a48cc93b31fbe9c85c4eecd18c8f9d6f
Alex André Romanhuk <romanhuk.x@gmail.com>
2026-02-10 22:30:08 -0300

    Estados

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Para gerar uma chave ssh-keygen.exe
 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbp1ZM0D00tuUPBKCwERPYCpBNMTAvlkBXXJ5VddmbX coamo\aaromanhuk@NCD312
 
 Instalar paconte de icons npm install lucide-react
+
+Todo nome que começar com useState "use" é um huck do react
--- react-course/src/App.tsx
+++ react-course/src/App.tsx
@@ -8,23 +8,28 @@ import { Button } from './components/Button';
 import { Footer } from './components/Footer';
 import { PlayCircleIcon } from 'lucide-react';
 import { Heading } from './components/Heading';
+import { useState } from 'react';
 
 import './styles/global.css';
 import './styles/theme.css'
 
+
 export function App() {
-    let numero  = 0;
+    //Sempre que eu usar useState, ele me retorna um array com 2 posições, a primeira é o valor do estado e a segunda é uma função para atualizar esse estado. 
+    // O useState é uma função que recebe o valor inicial do estado como argumento e retorna um array com o valor atual do estado e uma função para atualizar esse estado.
+    //Não usar atriuição diretamente no estado, pois isso não vai atualizar a interface do usuário. Sempre usar a função de atualização para garantir que a interface seja atualizada corretamente.
+    const [numero, setNumero] = useState(() => {
+        console.log('Valor inicial do estado');
+        return 0;
+    });
 
     function handleClick() {
-        numero++;
-        console.log(numero);
+        setNumero(prevState => prevState + 1);
     }
 
     return (
         <>
-            <Heading>
-                Número: {numero}
-            </Heading>
+            <Heading>Número: {numero}</Heading>
             <button onClick={handleClick}>Aumenta</button>
             <Container>
                 <Logo/>                
