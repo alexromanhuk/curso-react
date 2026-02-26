7b82ee0b9559a5e73e7092b81eb05faec37cba51
Alex André Romanhuk <romanhuk.x@gmail.com>
2026-02-25 22:18:57 -0300

    Estados

--- react-course/src/components/Menu/index.tsx
+++ react-course/src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -9,11 +9,26 @@ export function Menu(){
     const [theme, setTheme] = useState<AvailableThemes>('dark');
 
     function handleThemChanged(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
-        event.preventDefault();//Não segue o comportamento padrão do link
+        event.preventDefault();//Não segue o comportamento padrão do link, ou seja, não navega para lugar nenhum
         const newTheme = theme === 'dark' ? 'light' : 'dark';
         setTheme(newTheme);
         document.documentElement.setAttribute('data-theme', newTheme);
     }
+
+    //useEffect(() => {
+    //   console.log('Tema atualizado para:', theme);
+    //});//Exzecutado toda vez que o componente é renderizado, ou seja, toda vez que o estado 'theme' é atualizado
+
+    /*useEffect(() => {
+    //   console.log('useEffect com array deps vazio:', theme);
+    }, []);*///Executa apenas quanto o React monta  o componente na tela pela primeira vez, ou seja, quando o componente é renderizado pela primeira vez. Ele verifica se há um tema salvo no localStorage e, se houver, aplica esse tema ao estado e ao atributo data-theme do documento.
+
+
+    useEffect(() => {
+     // console.log('Tema atualizado para:', theme);
+      document.documentElement.setAttribute('data-theme', theme);
+      localStorage.setItem('theme', theme);
+    }, [theme]);//Executa apenas quanto o valor do theme é atualizado, ou seja, quando o usuário clica no link para mudar o tema. Ele salva o tema atual no localStorage para que a preferência do usuário seja mantida mesmo após recarregar a página ou fechar o navegador.
 
     return <div className={styles.menu}>
         <a className={styles.menuLink} href="#" aria-label='Ir para Home' title='Ir para a Home'>
